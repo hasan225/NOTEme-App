1babe3b4022ffd0f4ac9f315f439fc2772496a09
bijoy <uzumakibijoy2017@gmail.com>
2021-04-17 01:37:01 +0600

    NOTEme app is completed

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 // INSERT INTO `notes` (`sno`, `title`, `description`, `tstamp`) VALUES (NULL, 'buy books', 'buy the book which got number one place in the bookfair', current_timestamp());
 $insert = false;
+$update =false;
+$delete =false;
 const br = "<br>";
 //connect to the database
 
@@ -18,26 +20,53 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("wasn't connected successfully" . mysqli_connect_error());
 }
-//else {echo "connected successfully";}
+// echo ;
+// echo $_GET['update'];
+// exit();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //variable to be inserted into the table
-
-    $title = $_POST["title"];
-    $description = $_POST["description"];
-
-    //sql query to be executed
-
-    $sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
-
+if (isset($_GET['delete'])){
+    $sno = $_GET['delete'];
+    $delete=true;
+    $sql = "DELETE FROM `notes` WHERE `sno` = $sno";
     $result = mysqli_query($conn, $sql);
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['snoEdit'])) {
+        //update the record
 
-    //add new trip to the new trip table in the database
-    if ($result) {
-        // echo "the record was inserted successfully";
-        $insert = true;
+        $sno = $_POST["snoEdit"];
+        $title = $_POST["titleEdit"];
+        $description = $_POST["descriptionEdit"];
+
+        //sql query to be executed
+
+        $sql = "UPDATE `notes` SET `title` = '$title' , `description` = '$description' WHERE `notes`.`sno` = $sno";
+
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            $update=true;
+        } else {
+            "we failed to update";
+        }
     } else {
-        echo "the record wasn't inserted successfully because of this error " . mysqli_error($conn);
+        //variable to be inserted into the table
+
+        $title = $_POST["title"];
+        $description = $_POST["description"];
+
+        //sql query to be executed
+
+        $sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
+
+        $result = mysqli_query($conn, $sql);
+
+        //add new trip to the new trip table in the database
+        if ($result) {
+            // echo "the record was inserted successfully";
+            $insert = true;
+        } else {
+            echo "the record wasn't inserted successfully because of this error " . mysqli_error($conn);
+        }
     }
 }
 
@@ -59,16 +88,54 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="//cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#myTable').DataTable();
-        });
-    </script>
+
 
     <title>NOTEme -Easy And Reliable Note</title>
+
+
 </head>
 
 <body>
+
+    <!-- edit Modal -->
+
+    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">
+        Edit Modal
+    </button> -->
+
+    <!-- edit Modal -->
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Edit Note</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                    <form action="/CRUD%20Operations/" method="post">
+                <div class="modal-body">
+                        <input type="hidden" name="snoEdit" id="snoEdit">
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Note Title</label>
+                            <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Note Description</label>
+                            <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3"></textarea>
+                        </div>
+
+                       
+
+                </div>
+                <div class="modal-footer d-block mr-auto">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+                    </form>
+            </div>
+        </div>
+    </div>
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-info">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><i class="fab fa-intercom"></i> NOTEme</a>
@@ -122,10 +189,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     ?>
+    <?php
+
+    if ($delete) {
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+  <strong>Success!</strong> Your Note Has Been Deleted Successfully.
+  <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+</div>";
+    }
+
+    ?>
+    <?php
+
+    if ($update) {
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+  <strong>Success!</strong> Your Note Has Been Updated Successfully.
+  <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+</div>";
+    }
+
+    ?>
 
     <div class="container my-5">
-        <h2>Drop Your Note</h2>
-        <form action="/CRUD%20Operations/" method="post">
+        <h2>Drop Your Note On NOTEme</h2>
+        <form action="/CRUD%20Operations/index.php" method="post">
             <div class="mb-3">
                 <label for="title" class="form-label">Note Title</label>
                 <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
@@ -160,23 +247,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 $sql = "SELECT * FROM `notes`";
                 $result = mysqli_query($conn, $sql);
+                $sno = 0;
 
                 while ($row = mysqli_fetch_assoc($result)) {
+                    $sno = $sno + 1;
                     //echo var_dump($row);
                     echo "    <tr>
-                    <th scope='row'>" . $row['sno'] . "</th>
+                    <th scope='row'>" . $sno . "</th>
                     <td>" . $row['title'] . "</td>
                     <td>" . $row['description'] . "</td>
-                    <td>Action</td>
+                    <td><button class='edit btn btn-sm btn-primary' id=" . $row['sno'] . ">Edit</button>  <button class='delete btn btn-sm btn-primary' id=d" . $row['sno'] . ">Delete</button> </td>
                 </tr>";
                 }
 
 
                 ?>
+
             </tbody>
         </table>
 
     </div>
+
+    <hr>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -188,6 +280,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
     -->
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
+
+    <script>
+        edits = document.getElementsByClassName('edit');
+        Array.from(edits).forEach((element) => {
+            element.addEventListener("click", (e) => {
+                console.log("edit", );
+                tr = e.target.parentNode.parentNode;
+                title = tr.getElementsByTagName("td")[0].innerText;
+                description = tr.getElementsByTagName("td")[1].innerText;
+                console.log(title, description);
+                titleEdit.value = title;
+                descriptionEdit.value = description;
+                snoEdit.value = e.target.id;
+                console.log(e.target.id);
+                $('#editModal').modal('toggle');
+
+            })
+        })
+
+        deletes = document.getElementsByClassName('delete');
+        Array.from(deletes).forEach((element) => {
+            element.addEventListener("click", (e) => {
+                console.log("edit", );
+                sno = e.target.id.substr(1, );
+
+                if (confirm("Are You Sure You Want To Delete This Note!")) {
+                    console.log("yes")
+                    window.location = `/CRUD%20Operations/index.php?delete=${sno}`;
+                } else {
+                    console.log("no")
+                };
+
+            })
+        })
+    </script>
 </body>
 
 </html>
